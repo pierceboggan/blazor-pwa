aa68c0714ac75c38371c4ddbc2c169ef943c469a
Pierce Boggan <piboggan@microsoft.com>
2021-01-11 16:41:13 -0700

    Add Teams SDK

---
+++ Client/Teams/src/index.js
@@ -0,0 +1,11 @@
+﻿import * as microsoftTeams from "@microsoft/teams-js";
+
+export function init() {
+    microsoftTeams.initialize(window);
+}
+
+export function getUsername() {
+    microsoftTeams.getContext((context, error) => {
+        return Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+    });
+}
