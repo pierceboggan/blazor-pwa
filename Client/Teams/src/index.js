0d3a8b2eb258405c8f6483b025b649abc5c684bf
Pierce Boggan <piboggan@microsoft.com>
2021-01-11 16:55:52 -0700

    Ensure script included in Blazor app

--- Client/Teams/src/index.js
+++ Client/Teams/src/index.js
@@ -1,11 +1,14 @@
 ﻿import * as microsoftTeams from "@microsoft/teams-js";
 
 export function init() {
+    alert("hi");
     microsoftTeams.initialize(window);
 }
 
 export function getUsername() {
+    alert("hi2");
     microsoftTeams.getContext((context, error) => {
+        alert(Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : ""));
         return Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
     });
 }
