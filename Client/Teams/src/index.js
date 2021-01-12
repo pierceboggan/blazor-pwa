947dfd6b7c85bae30a52d82e6404a04c682aef5e
Pierce Boggan <piboggan@microsoft.com>
2021-01-11 18:33:42 -0700

    Fix bug

--- Client/Teams/src/index.js
+++ Client/Teams/src/index.js
@@ -3,12 +3,13 @@
 export function init() {
     alert("hi");
     microsoftTeams.initialize(window);
+    alert("hi again");
 }
 
 export function getUsername() {
     alert("hi2");
     microsoftTeams.getContext((context, error) => {
-        alert(Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : ""));
+        alert(Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "");
         return Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
     });
 }
